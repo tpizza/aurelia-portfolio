9b91ead54e474ae72237a9988bf7cac6cb625123
admin <theo.pizza@gmail.com>
2016-09-09 01:49:05 -0500

    Functional beta release: * Added form success messaging * Updated UI/UX for auto-form retraction on successful submit * Removed Steam link

--- src/contact/contact-panel.js
+++ src/contact/contact-panel.js
@@ -3,17 +3,26 @@ import {bindable} from 'aurelia-framework';
 export class ContactPanel {
 	
 	@bindable app;
+	formName = 'Portfolio Contact Form';
 	message = 'Say Hi!';
 	contactVisible = false;
 
 	toggleForm(animator){
-		this.contactVisible;
 		
 		if(animator === undefined)
-			throw new TypeError(`Please bind and pass the Animator from your View`);
+			throw new TypeError(`Please bind and pass the Animator`);
 	    
 	    let animationClass = this.contactVisible ? 'formFlyout' : 'formFlyin';
 	    animator.animateElement('.animatedFlyout', animationClass, this.contactVisible);
 	    this.contactVisible = this.contactVisible ? false : true;
+  	}
+
+  	closeForm(animator){
+  		console.info(this.contactVisible);
+  		if(animator === undefined)
+			throw new TypeError(`Please bind and pass the Animator`);
+	    
+	    animator.animateElement('.animatedFlyout', 'formFlyout', true);
+	    this.contactVisible = false;
   	}
 }
--- src/resources/elements/forms/contact-form.js
+++ src/resources/elements/forms/contact-form.js
@@ -8,9 +8,13 @@ import {CustomValidationRenderer} from '../../renderers/validation-renderer';
 @customElement('contact-form')
 export class ContactForm {
   
+  @bindable formName;
+  @bindable successCallback;
+  submitSuccess;
+  
   controller;
   client;
-
+  
   firstName;
   lastName;
   phoneNumber;
@@ -18,12 +22,10 @@ export class ContactForm {
   email;
   comment;
   ask;
-
   nature = [
     { id: 0, name: 'Inquiry'},
   	{ id: 1, name: 'Request'}
   ];
-
   contactMethod = [
   	{ id: 0, name: 'Email'},
   	{ id: 1, name: 'Phone'}
@@ -32,11 +34,13 @@ export class ContactForm {
   constructor(controllerFactory){
    
     this.activateRules();
-    this.controller = controllerFactory.createForCurrentScope();
+    
+    this.controller = controllerFactory.createForCurrentScope()
     this.controller.addRenderer(new CustomValidationRenderer());
+    
     this.client = new HttpClient()
       .configure( x => {
-        x.withBaseUrl('http://theopizza.com')
+        x.withBaseUrl('http://theopizza.com');
       });
 
   }
@@ -46,7 +50,7 @@ export class ContactForm {
     ValidationRules.customRule(
       'Mouthoff',
       (value, obj) => {
-        let valid = !(/damn/gi).test(value);
+        let valid = !(/damn|shit|fuck/gi).test(value);
         return valid;
       },
       `Watch your mouth!`
@@ -54,34 +58,54 @@ export class ContactForm {
 
     ValidationRules
       .ensure(ContactForm => firstName).required().minLength(2).satisfiesRule('Mouthoff').on(this)
-      .ensure(ContactForm => lastName).required().minLength(2).on(this)
-      .ensure(ContactForm => phoneNumber).required().when(ContactForm => this.preferredContact === 'Phone').on(this)
+      .ensure(ContactForm => lastName).required().minLength(2).satisfiesRule('Mouthoff').on(this)
+      .ensure(ContactForm => phoneNumber).required().when(ContactForm => (this.preferredContact === 'Phone' || !this.preferredContact)).satisfiesRule('Mouthoff').on(this)
       .ensure(ContactForm => email).email().required().when(ContactForm => this.preferredContact === 'Email').on(this);
-
   }
 
   submit(){
-    
+
     let errors = this.controller.validate();
-
     errors.then(v => {
+      
       if(v.length === 0){
-        console.info('passed');
+        let form = this;
         let formData = new FormData();
-        formData.append('formName', 'Portfolio Contact Form');
-        formData.append('firstName', this.firstName);
-        formData.append('lastName', this.lastName);
-        formData.append('email', this.email);
-        formData.append('phoneNumber', this.phoneNumber);
-        formData.append('comment', this.comment);
-        formData.append('preferredContactMethod', this.preferredContact);
-
-        console.info(formData);
-        this.client.post('request/ContactSubmit.php', formData); 
+          formData.append('formName', this.formName);
+          formData.append('firstName', this.firstName);
+          formData.append('lastName', this.lastName);
+          formData.append('email', this.email);
+          formData.append('phoneNumber', this.phoneNumber);
+          formData.append('comment', this.comment);
+          formData.append('preferredContactMethod', this.preferredContact);
         
-      } else {
-        console.info('error');
+        let request = this.client.createRequest('request/ContactSubmit.php')
+          .asPost()
+          .withContent(formData)
+          .withInterceptor({
+
+            requestError(err){
+              throw err;
+            },
+
+            responseError(err){
+              throw err;
+            },
+
+            response(http){
+              if(http.isSuccess){
+                form.submitSuccess = true;
+
+                //dirty check to make sure the callback array was bound
+                if(form.successCallback !== undefined)
+                  setTimeout ( () => form.successCallback[0].closeForm(form.successCallback[1]),2100 );
+              }
+            }
+
+          })
+          .send();
       }
+
     });
     
   }
